c9a5fbcee2963a44f4abf0229e5b9adb95ff1812
hugo-lafleur <troyes.lafleur.hugo@gmail.com>
2023-12-15 17:05:57 +0100

    add 2016/day03

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ For now, all of it is written in Go. Hopefully in the future, I would like to us
 |:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|
 |[day 01](./go/2015/day01/day01.go)|&check;|&check;|[day 01](./go/2016/day01/day01.go)|&check;|&check;|[day 01](./go/2022/day01/day01.go)|&check;|&check;|[day 01](./go/2023/day01/day01.go)|&check;|&check;|
 |[day 02](./go/2015/day02/day02.go)|&check;|&check;|[day 02](./go/2016/day02/day02.go)|&check;|&check;|[day 02](./go/2022/day02/day02.go)|&check;|&check;|[day 02](./go/2023/day02/day02.go)|&check;|&check;|
-|[day 03](./go/2015/day03/day03.go)|&check;|&check;||||[day 03](./go/2022/day03/day03.go)|&check;|&check;|[day 03](./go/2023/day03/day03.go)|&check;|&check;|
+|[day 03](./go/2015/day03/day03.go)|&check;|&check;|[day 03](./go/2016/day03/day03.go)|&check;|&check;|[day 03](./go/2022/day03/day03.go)|&check;|&check;|[day 03](./go/2023/day03/day03.go)|&check;|&check;|
 |[day 04](./go/2015/day04/day04.go)|&check;|&check;||||[day 04](./go/2022/day04/day04.go)|&check;|&check;|[day 04](./go/2023/day04/day04.go)|&check;|&check;|
 |[day 05](./go/2015/day05/day05.go)|&check;|&check;||||[day 05](./go/2022/day05/day05.go)|&check;|&check;|[day 05](./go/2023/day05/day05.go)|&check;|&check;|
 |[day 06](./go/2015/day06/day06.go)|&check;|&check;||||[day 06](./go/2022/day06/day06.go)|&check;|&check;|[day 06](./go/2023/day06/day06.go)|&check;|&check;|
---
+++ go/2016/day03/day03.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func format(s string) [][]int {
+	lines := strings.Split(s, "\n")
+	res := [][]int{}
+	for _, line := range lines {
+		strs := strings.Split(line, " ")
+		a, _ := strconv.Atoi(strs[0])
+		b, _ := strconv.Atoi(strs[1])
+		c, _ := strconv.Atoi(strs[2])
+		nbr := []int{}
+		nbr = append(nbr, a, b, c)
+		res = append(res, nbr)
+	}
+	return res
+}
+
+func part1(s string) int {
+	c := 0
+	lines := format(s)
+	for _, line := range lines {
+		if line[0]+line[1] > line[2] && line[2]+line[1] > line[0] && line[0]+line[2] > line[1] {
+			c++
+		}
+	}
+	return c
+}
+
+func part2(s string) int {
+	res := 0
+	lines := format(s)
+	i := 0
+	for i < len(lines)-2 {
+		j := 0
+		for j < 3 {
+			a := lines[i][j]
+			b := lines[i+1][j]
+			c := lines[i+2][j]
+			if a+b > c && a+c > b && b+c > a {
+				res++
+			}
+			j++
+		}
+		i = i + 3
+	}
+	return res
+}
+
+func main() {
+	content, err := os.ReadFile("test.data")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Test :\n")
+	start := time.Now()
+	fmt.Printf("\nPart 1 : %v\n", part1(string(content)))
+	fmt.Println(time.Since(start))
+	start = time.Now()
+	fmt.Printf("\nPart 2 : %v\n", part2(string(content)))
+	fmt.Println(time.Since(start))
+
+	content, err = os.ReadFile("input.data")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("\nInput :\n")
+	start = time.Now()
+	fmt.Printf("\nPart 1 : %v\n", part1(string(content)))
+	fmt.Println(time.Since(start))
+	start = time.Now()
+	fmt.Printf("\nPart 2 : %v\n", part2(string(content)))
+	fmt.Println(time.Since(start))
+}
