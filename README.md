7d75c6da9e937dfbd2d3c00d40eb4d4ac598b2e1
hugo-lafleur <troyes.lafleur.hugo@gmail.com>
2024-02-11 13:44:08 +0100

    add 2021/day07

--- README.md
+++ README.md
@@ -44,7 +44,7 @@ For now, all of it is written in Go. Hopefully in the future, I would like to us
 |[day04](./go/2019/day04/day04.go)|&check;|&check;|[day04](./go/2020/day04/day04.go)|&check;|&check;|[day04](./go/2021/day04/day04.go)|&check;|&check;|[day04](./go/2022/day04/day04.go)|&check;|&check;|
 |[day05](./go/2019/day05/day05.go)|&check;|&check;|[day05](./go/2020/day05/day05.go)|&check;|&check;|[day05](./go/2021/day05/day05.go)|&check;|&check;|[day05](./go/2022/day05/day05.go)|&check;|&check;|
 |[day06](./go/2019/day06/day06.go)|&check;|&check;|[day06](./go/2020/day06/day06.go)|&check;|&check;|[day06](./go/2021/day06/day06.go)|&check;|&check;|[day06](./go/2022/day06/day06.go)|&check;|&check;|
-|[day07](./go/2019/day07/day07.go)|&check;|&check;|[day07](./go/2020/day07/day07.go)|&check;|&check;|[day07](.)|||[day07](./go/2022/day07/day07.go)|&check;|&check;|
+|[day07](./go/2019/day07/day07.go)|&check;|&check;|[day07](./go/2020/day07/day07.go)|&check;|&check;|[day07](./go/2021/day07/day07.go)|&check;|&check;|[day07](./go/2022/day07/day07.go)|&check;|&check;|
 |[day08](./go/2019/day08/day08.go)|&check;|&check;|[day08](./go/2020/day08/day08.go)|&check;|&check;|[day08](.)|||[day08](./go/2022/day08/day08.go)|&check;|&check;|
 |[day09](./go/2019/day09/day09.go)|&check;|&check;|[day09](./go/2020/day09/day09.go)|&check;|&check;|[day09](.)|||[day09](./go/2022/day09/day09.go)|&check;|&check;|
 |[day10](./go/2019/day10/day10.go)|&check;|&check;|[day10](./go/2020/day10/day10.go)|&check;|&check;|[day10](.)|||[day10](./go/2022/day10/day10.go)|&check;|&check;|
---
+++ go/2021/day07/day07.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func format(s string) []int {
+	split := strings.Split(s, ",")
+	res := []int{}
+	for _, x := range split {
+		n, _ := strconv.Atoi(x)
+		res = append(res, n)
+	}
+	return res
+}
+
+func triangleSum(n int, cache map[int]int) int {
+	_, ok := cache[n]
+	if ok {
+		return cache[n]
+	}
+	if n == 0 {
+		return 0
+	}
+	cache[n] = n + triangleSum(n-1, cache)
+	return cache[n]
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func sumDiff(x int, list []int) int {
+	res := 0
+	for _, n := range list {
+		res += abs(n - x)
+	}
+	return res
+}
+
+func sumDiffTriange(x int, list []int, cache map[int]int) int {
+	res := 0
+	for _, n := range list {
+		res += triangleSum(abs(n-x), cache)
+	}
+	return res
+}
+
+func part1(s string) int {
+	list := format(s)
+	min, max := slices.Min(list), slices.Max(list)
+	minFuel := sumDiff(min, list)
+	for i := min; i <= max; i++ {
+		if sumDiff(i, list) < minFuel {
+			minFuel = sumDiff(i, list)
+		}
+	}
+	return minFuel
+}
+
+func part2(s string) int {
+	list := format(s)
+	min, max := slices.Min(list), slices.Max(list)
+	cache := make(map[int]int)
+	minFuel := sumDiffTriange(min, list, cache)
+	for i := min; i <= max; i++ {
+		if sumDiffTriange(i, list, cache) < minFuel {
+			minFuel = sumDiffTriange(i, list, cache)
+		}
+	}
+	return minFuel
+}
+func main() {
+	content, err := os.ReadFile("../../../inputs/2021/day07/test.data")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Test :\n")
+	start := time.Now()
+	fmt.Printf("\nPart 1 : %v\n", part1(string(content)))
+	fmt.Println(time.Since(start))
+	start = time.Now()
+	fmt.Printf("\nPart 2 : %v\n", part2(string(content)))
+	fmt.Println(time.Since(start))
+
+	content, err = os.ReadFile("../../../inputs/2021/day07/input.data")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("\nInput :\n")
+	start = time.Now()
+	fmt.Printf("\nPart 1 : %v\n", part1(string(content)))
+	fmt.Println(time.Since(start))
+	start = time.Now()
+	fmt.Printf("\nPart 2 : %v\n", part2(string(content)))
+	fmt.Println(time.Since(start))
+}
